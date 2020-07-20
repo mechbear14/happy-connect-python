8ccbccf24719853dc41d144bd2ccdac54adacbe3
Gabe Lyu <no6xichengavenue@gmail.com>
2020-07-20 03:46:01 +0100

    Added icons and code for loading icons

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import os
 
 from src.Core import Board
 from src.Sprites import BoardSprite
@@ -11,7 +12,12 @@ pygame.display.set_caption("Happy Connect")
 board_region = pygame.Surface((400, 400))
 board = Board(8, 8, 3).get_board()
 board_position = pygame.Vector2(0, 100)
-board_image = BoardSprite(board, board_region, 3)
+
+assets_path = os.path.dirname(os.path.realpath(__file__))
+icon1 = pygame.image.load(os.path.join(assets_path, "..", "assets", "icon1.png")).convert_alpha()
+icon2 = pygame.image.load(os.path.join(assets_path, "..", "assets", "icon2.png")).convert_alpha()
+icon3 = pygame.image.load(os.path.join(assets_path, "..", "assets", "icon3.png")).convert_alpha()
+board_image = BoardSprite(board, board_region, 3, [icon1, icon2, icon3])
 
 while True:
     for event in pygame.event.get():
